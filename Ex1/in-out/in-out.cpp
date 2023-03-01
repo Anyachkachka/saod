d8d131b2badce9fe06076517479648a052b744eb
Anna <nazarenkoanna2003@gmail.com>
2023-03-01 16:28:58 +0300

    zadanie 1

---
+++ Ex1/in-out/in-out.cpp
@@ -0,0 +1,24 @@
+﻿// in-out.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+	int x, y;
+	cout << "x = ? "; cin >> x; // Подсказка и ввод х
+	cout << "y = ? "; cin >> y; // Подсказка и ввод y
+	cout << x << " + " << y << " = " << x + y << endl;
+	cout << x << " * " << y << " = " << x * y << endl;
+	cout << x << " / " << y << " = " << x / y << endl;
+	cout << x << " % " << y << " = " << x % y << endl;
+	cout << x << " | " << y << " = " << (x | y) << endl;
+	cout << x << " & " << y << " = " << (x & y) << endl;
+	cout << x << " ^ " << y << " = " << (x ^ y) << endl;
+	cout << x << " << " << y << " = " << (x << y) << endl;
+	cout << x << " >> " << y << " = " << (x >> y) << endl;
+
+}
+
